fbf0f1796504d8ad88057ddbed94f8a3aa188890
Court Simas <courtsimas@gmail.com>
2012-05-23 16:11:25 -0500

    fixed some quote issues

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -48,7 +48,7 @@ module ViewHelpers
   
   def microsoft_fix_for_mobile
     javascript_tag("
-     (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
+     (function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener('click',function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;'href'in d&&(d.href.indexOf('http')||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,'standalone')
     ").html_safe
   end
   
